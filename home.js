13f7cc3ea35bb4c0883f8e4fe3355aa25657b588
oduguchidubem320-source <oduguchidubem320@gmail.com>
2026-07-30 16:09:32 +0100

    sixth

--- home.js
+++ home.js
@@ -1,7 +1,6 @@
 
-
   const menuIcon = document.querySelector('.menu-icon');
-  const dropdown = document.querySelector('.dropdown-menu');
+  const dropdown = document.querySelector('dropdown-menu')
 
   menuIcon.addEventListener('click', () => {
     dropdown.style.display = dropdown.style.display === 'flex' ? 'none' : 'flex';
